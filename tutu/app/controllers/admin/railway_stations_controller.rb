84ede561a06e397064596d47ae3e3655021dee6e
Valentun Sergeev <karbon4ic@gmail.com>
2017-05-19 19:06:13 +0600

    Localize stations

--- tutu/app/controllers/admin/railway_stations_controller.rb
+++ tutu/app/controllers/admin/railway_stations_controller.rb
@@ -21,7 +21,8 @@ class Admin::RailwayStationsController < Admin::BaseController
 
     if @railway_station.save
       redirect_to [:admin, @railway_station],
-                  notice: 'Railway station was successfully created.'
+                  notice: I18n.t('common.statuses.created',
+                                 resource: @railway_station.localize)
     else
       render :new
     end
@@ -30,7 +31,8 @@ class Admin::RailwayStationsController < Admin::BaseController
   def update
     if @railway_station.update(railway_station_params)
       redirect_to [:admin, @railway_station],
-                  notice: 'Railway station was successfully updated.'
+                  notice: I18n.t('common.statuses.updated',
+                                 resource: @railway_station.localize)
     else
       render :edit
     end
@@ -40,15 +42,17 @@ class Admin::RailwayStationsController < Admin::BaseController
     @railway_station.destroy
 
     redirect_to admin_railway_stations_url,
-                notice: 'Railway station was successfully destroyed.'
+                notice: I18n.t('common.statuses.destroyed',
+                               resource: @railway_station.localize)
   end
 
   def update_route_params
     @route = Route.find(params[:route_id])
     if @railway_station.update_route_params(@route, station_route_params)
-      redirect_to [:admin, @route], notice: 'Position was updated'
+      redirect_to [:admin, @route],
+                  notice: I18n.t('admin.railway_stations.params_upadted')
     else
-      redirect_to [:admin, @route], alert: 'Invalid params.'
+      redirect_to [:admin, @route], alert: I18n.t('common.statuses.rejected')
     end
   end
 
