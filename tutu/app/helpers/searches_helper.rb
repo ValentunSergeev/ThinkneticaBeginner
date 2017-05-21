60e5c51b1ff439b9e4c68f4e4396faa22245a3cb
Valentun Sergeev <karbon4ic@gmail.com>
2017-05-21 12:09:55 +0600

    Design for user part

--- tutu/app/helpers/searches_helper.rb
+++ tutu/app/helpers/searches_helper.rb
@@ -6,4 +6,12 @@ module SearchesHelper
       'col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2'
     end
   end
+
+  def query_params
+    result = {}
+    result[:start_station_id] = params[:start_station_id] || @start_station_id
+    result[:end_station_id] = params[:end_station_id] || @end_station_id
+    result[:anchor] = "train-#{params[:train_id]}"
+    result
+  end
 end
