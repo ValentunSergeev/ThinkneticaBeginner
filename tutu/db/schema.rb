92f4ef68e06789e0cf879e158c52edd8136edc50
Valentun Sergeev <karbon4ic@gmail.com>
2017-05-23 18:33:39 +0600

    Add indexes

--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516144421) do
+ActiveRecord::Schema.define(version: 20170523122837) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170516144421) do
     t.integer  "sitting_seats",     default: 0
     t.string   "type"
     t.integer  "number"
+    t.index ["id", "type"], name: "index_wagons_on_id_and_type"
     t.index ["train_id"], name: "index_wagons_on_train_id"
   end
 
